36d7059a63cf2a92d0d3dba4a9a1567530b89455
lida-skakun <kunska@gmail.com>
2016-09-08 14:59:40 +0300

    Feedback

--- js/components/contacts.jsx
+++ js/components/contacts.jsx
@@ -5,11 +5,16 @@ export default class Contacts extends React.Component {
     render () {
         return 	<div id="contacts">
             <h1>How you can find us</h1>
-            <iframe src="https://www.google.com/maps/d/u/1/embed?mid=1FGgduyteUSDMjox1YljQvNnAhPk" width="640" height="480"></iframe>
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.  </p>
+            <iframe src="https://www.google.com/maps/d/u/1/embed?mid=1FGgduyteUSDMjox1YljQvNnAhPk"></iframe>
+            <div id="address">
+                <h2> Our location</h2>
+                <br>Kiev, Ukraine <br/>
+                    Eastern Europe <br/>
+                    cell: +38 (093) 914 - 26 - 46 <br/>
+                    e-mail: kunska@gmail.com</br>
+                     
+                </p>
+            </div>
 
             <footer>
                 Made in Ukraine<br/>
--- js/components/home.jsx
+++ js/components/home.jsx
@@ -31,22 +31,49 @@ export default class Home extends React.Component {
                     <div id="header-img" className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <div className="header-img">
                             <img className="img-responsive" src="Hojmft2FmnM.jpg" alt="header-img" />
+                            <button type="button" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal"> <img id="feedback" src="social_balloon-37.png"/> </button>
                         </div>
                     </div>
                 </div>
             </div>
-
+            <div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span className="sr-only">Cencel</span></button>
+                            <h4 className="modal-title" id="myModalLabel">Feedback</h4>
+                        </div>
+                        <div className="modal-body">
+                            <form className="form-horizontal" role="form">
+                                <div className="form-group">
+                                    <label htmlFor="inputName" className="col-sm-2 control-label">Name</label>
+                                    <div className="col-sm-10">
+                                        <input type="text" className="form-control" id="inputName" placeholder="Name"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="inputEmail" className="col-sm-2 control-label">Email</label>
+                                    <div className="col-sm-10">
+                                        <input type="email" className="form-control" id="inputEmail3" placeholder="Email"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="inputPhone" className="col-sm-2 control-label">Phone</label>
+                                    <div className="col-sm-10">
+                                        <input type="phone" className="form-control" id="inputPhone" placeholder="Phone"/>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">Cencel</button>
+                            <button type="button" className="btn btn-primary">Send</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <a href="index.html#home" title="Up">  <img id="arrow" src="icons/137.png"/>  </a>
         </div> ;
         }
     }
 
-/* <div id="callback">
- <div class="container-fluide">
- <div class="row">
- <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
- <img src="mail.png" id="feedback" title="Feedback"/>
- </div>
- </div>
- </div>
- </div> */
